49f46defe5566500a963c5a840d239c0ee4cc8e5
Aaoz Gull <aaozgull@gmail.com>
2024-02-16 23:09:32 +0500

    starring messages

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -121,6 +121,10 @@ const ChatScreen = (props) => {
                   return <Bubble
                             type={messageType}
                             text={message.text}
+                            messageId={message.key}
+                            userId={userData.userId}
+                            chatId={chatId}
+                            date={message.sentAt}
                           />
                 }}
               />
